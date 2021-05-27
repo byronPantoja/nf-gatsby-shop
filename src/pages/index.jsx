d9f6cf9b8226b71a3719a6a3d9b1bd1703d1b32d
“dbp” <byronpantoja@gmail.com>
2021-05-28 05:54:05 +0800

    customizing index.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,7 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import { Layout } from "../components/layout"
 import { ProductListing } from "../components/product-listing"
-import {
-  container,
-  intro,
-  callOut,
-  callToAction,
-  deployButton,
-} from "./index.module.css"
+import { container, intro, callOut } from "./index.module.css"
 
 export const query = graphql`
   query {
@@ -23,22 +17,11 @@ export default function IndexPage({ data }) {
   return (
     <Layout>
       <div className={container}>
-        <h1 className={intro}>Welcome to the GatsbyJS + Shopify Demo Store.</h1>
+        <h1 className={intro}>
+          Welcome to our Notes and Flowers GatsbyJS + Shopify Demo Build.
+        </h1>
         <p className={callOut}>
-          It's a proof-of-concept in a box, with 10k products and 30k variants
-          to help you get to proof-of-concept as soon as right now.
-        </p>
-        <p className={callToAction}>
-          Hook it up to your own Shopify store data and start customizing in
-          minutes by deploying it to Gatsby Cloud for free. Grab your Shopify
-          store credentials and
-          <a href="https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/gatsbyjs/gatsby-starter-shopify&utm_campaign=shopify-starter">
-            <img
-              src="https://www.gatsbyjs.com/deploynow.png"
-              alt="Deploy to Gatsby Cloud"
-              className={deployButton}
-            />
-          </a>
+          So this is the first time I got to build on the cloud, pretty cool!
         </p>
       </div>
       <ProductListing products={data.shopifyCollection.products} />
